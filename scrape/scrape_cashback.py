69934113f47161827a397b7e5df4c6fbc215dd3c
BrandonTay2001 <brandontay2001@gmail.com>
2023-12-16 18:43:20 +0800

    first draft of adding to db

--- scrape/scrape_cashback.py
+++ scrape/scrape_cashback.py
@@ -6,7 +6,12 @@ import pymongo
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+client = pymongo.MongoClient(os.environ.get("MONGO_URL"))
+db = client["cardCalc"]
+col = db["cashback"]
 
 url = "https://ringgitplus.com/en/credit-card/cashback/"
 
@@ -23,5 +28,25 @@ for li in li_tags:
     link = "https://ringgitplus.com" + a_tag["href"]
     name = a_tag.text
     links_and_names[name] = link
-    card_info = scrape_individual(link)
-    print(card_info)
+    rows = scrape_individual(link)
+    
+    seen = set()
+    
+    # if name is not in db, add it
+    if col.find_one({"name": name}) == None:
+        col.insert_one({"name": name, "link": link, "categories": {}})
+
+    categories = {}
+
+    for row in rows:
+        # seen key: (categories, spend)
+        if (tuple(row[0]), row[3]) in seen:
+            continue
+        seen.add((row[0], row[3]))
+        
+        if tuple(row[0]) in categories:
+            categories[tuple(row[0])].append({"cashback_percentage": row[1], "monthly_cap": row[2], "spend": row[3]})
+        else:
+            categories[tuple(row[0])] = [{"cashback_percentage": row[1], "monthly_cap": row[2], "spend": row[3]}]
+        
+    col.update_one({"name": name}, {"$set": {"categories": categories}})
